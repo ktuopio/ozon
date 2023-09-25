08f3836913f016ec9bf2b3d6ddb2b9cc930ab558
ktuopio <143160095+ktuopio@users.noreply.github.com>
2023-09-25 15:59:15 +0300

    Add files via upload
    Download pdf files from .pdf link with customized name

---
+++ download links.py
@@ -0,0 +1,39 @@
+import requests
+import os
+
+# Список ссылок на PDF-файлы
+pdf_links = [
+"https://link1.pdf",
+"https://link2.pdf",
+    # Добавьте остальные ссылки
+]
+
+# Список наименований, в том порядке, в котором они соответствуют PDF-файлам
+pdf_names = [
+"name1",
+"name2",
+    # Добавьте остальные наименования
+]
+
+# Заголовок User-Agent
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+}
+
+# Создание папки для сохранения файлов, если её нет
+if not os.path.exists(r"C:\foldername"):
+    os.makedirs(r"C:\foldername")
+
+# Скачивание и сохранение файлов
+for link, name in zip(pdf_links, pdf_names):
+    response = requests.get(link, headers=headers, verify=False)
+    file_path = os.path.join(r"C:\foldername", f"{name}.pdf")
+
+    # Проверка, существует ли уже файл с таким именем, и создание копии при необходимости
+    counter = 1
+    while os.path.exists(file_path):
+        file_path = os.path.join(r"C:\foldername", f"{name}_{counter}.pdf")
+        counter += 1
+
+    with open(file_path, "wb") as f:
+        f.write(response.content)
